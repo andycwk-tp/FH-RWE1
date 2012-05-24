1d000d0c0e9ec9a569a058e5eed06b044f5ad2fe
unknown <amacdonald@technophobia.com>
2012-05-24 16:31:19 +0100

    tweak to client side js

--- client/default/scripts/main.js
+++ client/default/scripts/main.js
@@ -1,11 +1,11 @@
 $().ready(function(){
 	$('form#report-form').on('submit', function(e){
 		$fc.act({
-			act: 'processRequest'
+			act: 'processReport'
 		}, function (){
-			alert(arguments);
+			alert(args);
 		}, function (){
-			alert(arguments);
+			alert(args);
 		}
 		);
 		e.preventDefault();
