ac4187702419c863bb9801f6060e81755c7f589e
amacdonald@technophobia.com <amacdonald@technophobia.com>
2012-05-29 08:25:45 +0000

    update file /client/default/scripts/main.js

--- client/default/scripts/main.js
+++ client/default/scripts/main.js
@@ -2,7 +2,8 @@ $().ready(function(){
 	$('form#report-form').on('submit', function(e){    
     e.preventDefault();
 		$fh.act({
-			act: 'processReport'
+			act: 'processReport',
+      req: {data: 'test', mode:'cool'}
 		}, function (){
 			alert(arguments);      
 		}, function (){
