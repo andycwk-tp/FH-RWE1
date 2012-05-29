f2db041aac8110a32d799725a3161a7364952470
amacdonald@technophobia.com <amacdonald@technophobia.com>
2012-05-29 08:42:23 +0000

    update file /client/default/scripts/main.js

--- client/default/scripts/main.js
+++ client/default/scripts/main.js
@@ -2,17 +2,17 @@ $().ready(function(){
 	$('form#report-form').on('submit', function(e){    
     e.preventDefault();
     try {
-  		$fh.act({
-  			act: 'processReport',
-        req: {data: {nifty:'test', mode:'cool'})
-  		}, function (){
-  			alert(arguments);      
-  		}, function (){
-  			alert('ooops');
-  		}
-  		);
-    } catch (err){
-      alert(err.message);
+      $fh.act({
+        act: 'processReport',
+        req: {data: 'test'}
+      },function(){
+        alert('pass');
+      }, function(){
+        alert('fail');
+      });
+    } catch(err) {
+        alert(err.message);
+      }
     }
 	});
 })
