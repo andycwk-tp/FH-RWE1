b2d7cbc0efe14ea49bd6ae07a9e8c9a99f918a46
unknown <amacdonald@technophobia.com>
2012-05-29 11:12:56 +0100

    added form serialization

--- client/default/scripts/main.js
+++ client/default/scripts/main.js
@@ -1,15 +1,29 @@
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
 $().ready(function(){
 	$('form#report-form').on('submit', function(e){    
     e.preventDefault();
     try {
 		var form = $('form#report-form');
-		alert(form['firstName']);
+		console.log(form.serializeObject());
 		$fh.act({
 			act: 'processReport',
-			req: {
-				formData: {
-					firstName: form['firstName']
-				}}
+			req: {formData: form.serializeObject()}
 		},function(){
 			alert('pass');
 		}, function(msg, err){
