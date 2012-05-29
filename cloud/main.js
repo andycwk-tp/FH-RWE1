6198fca29966fc6b24a3dcbebf6f37f62bb2edde
amacdonald@technophobia.com <amacdonald@technophobia.com>
2012-05-29 10:33:31 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,7 +2,7 @@ function processReport(param){
   try {
 	var reportLogEntry = 'New report submitted... ';
 	for (var index in param.formData){
-		reportLogEntry += index + ':' + param.formData[index]) + '  ';
+		reportLogEntry += index + ':' + param.formData[index] + '  ';
 	}
 	$fh.log(reportLogEntry);
 	
