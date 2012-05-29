5649863f2cbb6f7bd38bf12adeaa4769873a3200
unknown <amacdonald@technophobia.com>
2012-05-29 11:17:23 +0100

    added code to cloud to log form data

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,6 @@
 function processReport(param){
   try {
-	  $fh.log(param.formData.firstName, param.formData.lastName, param.formData.emailAddress);
+	  $fh.log(param.formData.firstName + '  ' + param.formData.lastName + '  ' + param.formData.emailAddress);
   } catch(err) {
     $fh.log(err.message);
     throw "paramater data is wrong";
