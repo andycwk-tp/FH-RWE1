fb0b303718758ab4d317d82ea10a892254eed153
Oliver <ocaine@technophobia.com>
2012-06-18 15:56:56 +0100

    adding map

--- client/default/scripts/main.js
+++ client/default/scripts/main.js
@@ -14,84 +14,102 @@ $.fn.serializeObject = function()
     });
     return o;
 };
+$().ready(function () {
+    var setLocation = function () {
+        try {
+            $fh.geo({
+                interval: 0
+            }, function (result) {
+                $('#reportLocationLat').val(result.lat);
+                $('#reportLocationLong').val(result.lon);
+            }, function (message) {
+                //$('#reportMyLocation').parent().parent().remove();
+                alert(message);
+            });
+        } catch (err) {
+            alert('geo-location call not supported - disabling geo-location check (' + err + ')');
+            //$('#reportMyLocation').parent().parent().remove();
+        }
+    };
 
-$().ready(function(){
-	var setLocation = function(){
-		try {
-			$fh.geo({
-				interval: 0				
-			}, function(result){
-				$('#reportLocationLat').val(result.lat);
-				$('#reportLocationLong').val(result.lon);
-			}, function(message){
-				//$('#reportMyLocation').parent().parent().remove();
-				alert(message);
-			});
-		} catch(err) {
-			alert('geo-location call not supported - disabling geo-location check ('+ err +')');
-			//$('#reportMyLocation').parent().parent().remove();
-		}
-	};
+    try {
+        $fh.geo({
+            interval: 0
+        }, function (res) {
+            $fh.map({
+                target: '#map',
+                lat: res.lat,
+                lon: res.lon,
+                zoom: 9
+            }, function (map) {
+                console.log(map.map);
+            }, function (msg, err) {
+                console.log(msg);
+            });
+        });
+    } catch (err) {
+        alert("google maps not supported (" + err + ")");
+    }
 
-	setTimeout(function(){setLocation();}, 50);
-	$('form#report-form').on('submit', function(e){    
-		e.preventDefault();
-		try {
-			var form = $('form#report-form');
-			console.log(form.serializeObject());
-			$fh.act({
-				act: 'processReport',
-				req: {formData: form.serializeObject()}
-			},function(result){
-				alert(result.result);
-			}, function(msg, err){
-				alert(msg + ' ' + err.message + err.error);
-			});
-			} catch(err) {
-				alert('catch: ' + err.message);
-		}
-	});
-	
-	$('#reportMyLocation').on('click', function(e){
-		var isChecked = $(this).attr('checked') === 'checked';
-		if (isChecked) {
-			alert('set');
-			setLocation();
-		} else {
-			alert('clear');
-			$('#reportLocationLat').val('');
-			$('#reportLocationLong').val('');
-		}
-	});
-	
-	$('#takePhoto, #useGallery').on('click', function(){
-		var photoFrame = $('#reportPhoto');
-		var oldSource = photoFrame.attr('src');
-		var oldBase64Data = $('#reportPhoto').val();
-		setTimeout(function(){
-			photoFrame.attr('src', 'graphics/progress.gif');
-		}, 50);
-		var source = this.id === 'takePhoto'
+    setTimeout(function () { setLocation(); }, 50);
+    $('form#report-form').on('submit', function (e) {
+        e.preventDefault();
+        try {
+            var form = $('form#report-form');
+            console.log(form.serializeObject());
+            $fh.act({
+                act: 'processReport',
+                req: { formData: form.serializeObject() }
+            }, function (result) {
+                alert(result.result);
+            }, function (msg, err) {
+                alert(msg + ' ' + err.message + err.error);
+            });
+        } catch (err) {
+            alert('catch: ' + err.message);
+        }
+    });
+
+    $('#reportMyLocation').on('click', function (e) {
+        var isChecked = $(this).attr('checked') === 'checked';
+        if (isChecked) {
+            alert('set');
+            setLocation();
+        } else {
+            alert('clear');
+            $('#reportLocationLat').val('');
+            $('#reportLocationLong').val('');
+        }
+    });
+
+    $('#takePhoto, #useGallery').on('click', function () {
+        var photoFrame = $('#reportPhoto');
+        var oldSource = photoFrame.attr('src');
+        var oldBase64Data = $('#reportPhoto').val();
+        setTimeout(function () {
+            photoFrame.attr('src', 'graphics/progress.gif');
+        }, 50);
+        var source = this.id === 'takePhoto'
 			? 'camera'
-			: 'photo';		
-		try {
-			$fh.cam({
-				act: 'picture',
-				source: source,
-				uri: false
-			}, function(result){
-				var photoData = 'data:image/' + result.format + ';base64,' + result.b64;
-				$('#reportPhotoData').val(result.b64);
-				$('#reportPhoto').attr('src', photoData);
-			}, function(msg, err){
-				photoFrame.attr('src', oldSource);	
-				$('#reportPhoto').val(oldBase64Data);
-				alert('fault:  msg' + msg);
-			});
-		} catch(err) {
-			photoFrame.attr('src', oldSource);
-			$('#reportPhoto').val(oldBase64Data);
-			alert('cammera not supported - photo options ('+ err +')');			
-		}
-	});
+			: 'photo';
+        try {
+            $fh.cam({
+                act: 'picture',
+                source: source,
+                uri: false
+            }, function (result) {
+                var photoData = 'data:image/' + result.format + ';base64,' + result.b64;
+                $('#reportPhotoData').val(result.b64);
+                $('#reportPhoto').attr('src', photoData);
+            }, function (msg, err) {
+                photoFrame.attr('src', oldSource);
+                $('#reportPhoto').val(oldBase64Data);
+                alert('fault:  msg' + msg);
+            });
+        } catch (err) {
+            photoFrame.attr('src', oldSource);
+            $('#reportPhoto').val(oldBase64Data);
+            alert('cammera not supported - photo options (' + err + ')');
+        }
+    });
 });
