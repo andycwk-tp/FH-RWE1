f9d1bca84d4e417787b44a2d8f693a31f7d901fe
amacdonald@technophobia.com <amacdonald@technophobia.com>
2012-06-22 13:45:33 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,8 +3,8 @@ require('util');
 exports.processReport = function(params, callback){
 	try {
 		var reportLogEntry = 'New report submitted... ';
-		for (var index in param.formData){
-			reportLogEntry += index + ':' + param.formData[index] + '  ';
+		for (var index in params.formData){
+			reportLogEntry += index + ':' + params.formData[index] + '  ';
 		}
 		$fh.log(reportLogEntry);
 		return callback(null,{result: reportLogEntry});
